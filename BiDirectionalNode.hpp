fc96810f31493be098622a5cc8acebd86a1feef8
thenaz <ammarznazir@gmail.com>
2017-03-03 09:54:23 -0700

    Added getters and setters to BiDirectionalNode.hpp

--- BiDirectionalNode.hpp
+++ BiDirectionalNode.hpp
@@ -13,6 +13,7 @@ template <class Type>
 class BiDirectionalNode : public Node<Type>
 {
 private:
+//  Type data; Removed as the data member will be inherited from Node<Type>
     Type data;
     BiDirectionalNode<Type> * nextPointer;
     BiDirectionalNode<Type> * previousPointer;
@@ -21,28 +22,57 @@ public:
     BiDirectionalNode(Type data);
     BiDirectionalNode(Type data, BiDirectionalNode<Type> * previous, BiDirectionalNode<Type> * next);
     Type getNodeData();
+    
+//  Type getNodeData();
     BiDirectionalNode<Type> * getNextPointer();
     BiDirectionalNode<Type> * getPreviousPointer();
     
-    void setNodeData(Type data);
+//  void setNodeData(Type data);
     void setNextPointer(BiDirectionalNode<Type> * next);
     void setPreviousPointer(BiDirectionalNode<Type> * previous);
     
 };
 
 template <class Type>
-BiDirectionalNode :: BiDirectionalNode() : Node()
+BiDirectionalNode :: BiDirectionalNode() : Node<Type>()
 {
-    
+    this->nextPointer = nullptr;
+    this->previous = nullptr;
 }
 
 
 template <class Type>
 BiDirectionalNode :: BiDirectionalNode(Type data) : Node(data)
 {
-    
+    this->nextPointer = nullptr;
+    this->previous = nullptr;
 }
 
+template <class Type>
+BiDirectionalNode<Type> :: BiDirectionalNode(Type data, BiDirectionalNode<Type> * previous, BiDirectionalNode<Type> * next) ; Node(data, next)
+{
+    this->nextPointer = next;
+    this->previous = previous;
+}
+
+template <class Type>
+BiDirectionalNode<Type> * BiDirectionalNode<Type> :: getNextPointer()
+{
+    return this->previous;
+}
+
+template <class Type>
+void BiDirectionalNode<Type> * BiDirectionalNode<Type> :: setNextPointer(BiDirectionalNode<Type> * next)
+{
+    this->previous = next;
+}
+
+
+template <class Type>
+void BiDirectionalNode<Type> * BiDirectionalNode<Type> :: setNextPointer(BiDirectionalNode<Type> * next)
+{
+    this->previous = previous;
+}
 
 
 
